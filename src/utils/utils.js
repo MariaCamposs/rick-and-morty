9ab4f48217492d8414e3550c9c91acad9dba8c1e
MariaCamposs <mariacampos@gmail.com>
2021-12-06 22:04:40 -0500

    Add async function to extract count from endpoint

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch';
 
 export default {
-  count: async(endpoint) => {
-    const route = `https://rickandmortyapi.com/api/${endpoint}/`;
+  count: async(resource) => {
+    const endpoint = `https://rickandmortyapi.com/api/${resource}/`;
     let count;
     try {
-      const request = await fetch(route);
+      const request = await fetch(endpoint);
       const json = await request.json();
       count = json.info.count;
     } catch (error) {
